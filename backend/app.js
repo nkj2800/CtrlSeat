5475134fbaac79f3a50a0c9935192e578f93ccbd
Noel <nkj2800@gmail.com>
2025-04-10 14:15:32 +0530

    feat(errorHandling): implement centralized error handling and graceful shutdown
    - Create custom ErrorHandler class for consistent error structure
    - Add error-handling middleware to manage API errors
    - Set up listeners for uncaught exceptions and unhandled promise rejections

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,40 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 
 import { connectDatabase } from './config/dbConnect.js'
+import errorMiddleware from './middlewares/error.js'
 
+// Handle Uncaught Exceptions
+process.on('uncaughtException', (err)=> {
+  console.log(`Error: ${err}`)
+  console.log('Shutting down due to Uncaught Exception')
+
+  process.exit(1)
+})
 
 const app = express()
 
 dotenv.config({ path: './backend/config/config.env' })
 
+app.use(express.json())
+app.use(morgan('dev'))
+
 connectDatabase()
 
 
-app.listen(process.env.PORT, () => {
+app.use(errorMiddleware)
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server listening on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
 })
+
+// Handle Unhandled Promise Rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err}`)
+  console.log('Shutting down server due to Unhandled Promise Rejection');
+
+  server.close(() => {
+    process.exit(1)
+  })
+}) 
